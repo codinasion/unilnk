b79b3a0d789ac26d0a96ed7b33921de0221a98b3
Harsh Raj <harshraj8843@gmail.com>
2025-02-15 12:49:07 +0530

    Refactor URL routing and improve template formatting for consistency

--- main/views.py
+++ main/views.py
@@ -10,19 +10,23 @@ class HomeView(View):
 class CategoriesView(View):
     def get(self, request):
         return render(request, "categories.html")
-    
+
+
 class CategoryItemsView(View):
     def get(self, request, category_str):
         return render(request, "category-items.html")
-    
+
+
 class SearchView(View):
     def get(self, request):
         return render(request, "search.html")
-    
+
+
 class ItemView(View):
     def get(self, request, item_slug):
         return render(request, "item.html")
-    
+
+
 class NewItemView(View):
     def get(self, request):
         return render(request, "new-item.html")
